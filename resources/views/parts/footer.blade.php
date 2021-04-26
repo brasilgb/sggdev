b3665314d10b108e2dffa109a87fe0d960275802
Anderson Rodrigues <andersonbrasil@outlook.com>
2021-04-25 23:26:49 -0300

    Push 25-04-2021 23-26

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="footer mt-auto pt-2 shadown-sm">
+<footer class="footer mt-auto pb-2 pt-2 shadown-sm">
     <div class="container text-center">
         <span>Copyright © {{ date('Y') }} <a href="https://abrasildigital.com.br/">ABrasil Digital</a>. Todos
             os
